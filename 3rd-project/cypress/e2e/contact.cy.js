f784333ab3ec9b986bcd2424107eff0b4827b998
Linsted <ivan.malenko18@gmail.com>
2024-02-19 21:36:45 +0200

    feat: finish section 3, more deep knowledge

--- 3rd-project/cypress/e2e/contact.cy.js
+++ 3rd-project/cypress/e2e/contact.cy.js
@@ -27,8 +27,62 @@ describe("Contact page", () => {
     //   then() method
 
     cy.get('[data-cy="contact-btn-submit"]').then((btn) => {
-      expect(btn.attr("disabled").to.be.undefined);
+      // expect(btn.attr("disabled")).to.be.undefined;
       expect(btn.text()).to.equal("Sending...");
     });
+  });
+
+  it("should submit form with Enter key", () => {
+    cy.visit("http://localhost:5173/about");
+
+    cy.get('[data-cy="contact-input-message"]').type("Hello, World!");
+    cy.get('[data-cy="contact-input-name"]').type("John Doe");
+    cy.get('[data-cy="contact-input-email"]').type("mail@mail.com{enter}");
+
+    cy.get('[data-cy="contact-btn-submit"]').click();
+
+    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
+  });
+
+  it("should validate form ", () => {
+    cy.visit("http://localhost:5173/about");
+
+    cy.get('[data-cy="contact-btn-submit"]')
+      .click()
+      .then((el) => {
+        expect(el).to.not.have.attr("disabled");
+        expect(el.text()).to.not.equal("Sending...");
+      });
+
+    // cy.get('[data-cy="contact-input-message"]')
+    //   .focus()
+    //   .blur()
+    //   .parent()
+    //   .then((el) => {
+    //     expect(el.attr("class")).to.contains("invalid");
+    //   });
+
+    cy.get('[data-cy="contact-input-message"]')
+      .focus()
+      .blur()
+      .parent()
+      .should("have.attr", "class")
+      .and("match", /invalid/);
+
+    // cy.get('[data-cy="contact-input-name"]')
+    //   .focus()
+    //   .blur()
+    //   .parent()
+    //   .then((el) => {
+    //     expect(el.attr("class")).to.contains("invalid");
+    //   });
+
+    // cy.get('[data-cy="contact-input-email"]')
+    //   .focus()
+    //   .blur()
+    //   .parent()
+    //   .then((el) => {
+    //     expect(el.attr("class")).to.contains("invalid");
+    //   });
   });
 });
