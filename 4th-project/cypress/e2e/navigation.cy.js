fba9d1cfccd83e4b25c5ff35653bb514f6b90240
Linsted <ivan.malenko18@gmail.com>
2024-02-22 14:27:59 +0200

    feat: finish

--- 4th-project/cypress/e2e/navigation.cy.js
+++ 4th-project/cypress/e2e/navigation.cy.js
@@ -1,14 +1,18 @@
 /// <reference types="Cypress" />
 
-describe('page navigation', () => {
-  it('should navigate between pages', () => {
-    cy.visit('http://localhost:5173/');
+describe("page navigation", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    // base url added to cypress.config.js
+  });
+
+  it("should navigate between pages", () => {
     cy.get('[data-cy="header-about-link"]').click();
-    cy.location('pathname').should('eq', '/about'); // /about => About page
-    cy.go('back');
-    cy.location('pathname').should('eq', '/'); // / => Home page
+    cy.location("pathname").should("eq", "/about"); // /about => About page
+    cy.go("back");
+    cy.location("pathname").should("eq", "/"); // / => Home page
     cy.get('[data-cy="header-about-link"]').click();
     cy.get('[data-cy="header-home-link"]').click();
-    cy.location('pathname').should('eq', '/'); // / => Home page
+    cy.location("pathname").should("eq", "/"); // / => Home page
   });
 });
